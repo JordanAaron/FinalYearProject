12204314c42517e3a5c1dd0a4fd898357f44c2ab
Jordan <jordev14@gmail.com>
2020-04-03 09:22:37 +0100

    Implemented client and gameserver class to join the server and process messages sent by the server.

---
+++ FabledKingdoms/src/Client/Client.java
@@ -0,0 +1,49 @@
+package Client;
+
+
+import Maps.TestingMap;
+
+
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class Client /*implements AutoCloseable*/{
+    final int port = 8888;
+
+    Socket socket = new Socket("localhost", port);
+    private GameServer gameServer;
+
+    TestingMap testingMap;
+
+    public Client() throws Exception {
+        //mapSelection("TestingMap");
+
+    }
+
+    //make it so that if anything changes on the screen that's when you send a message
+
+    public void playerCountRequest(){
+        gameServer.send("PLAYERCOUNT");
+    }
+
+    public void playerMovement(int xPos, int yPos) {
+        gameServer.send("POSITION " + xPos + " " + yPos);
+    }
+
+    public void mapSelection(String map){
+        gameServer.send("MAP " + map);
+    }
+
+    public void giveMeMapReference(TestingMap testingMap) throws IOException {
+        this.testingMap = testingMap;
+
+        gameServer = new GameServer(socket, this.testingMap);
+        Thread thread = new Thread(gameServer);
+        thread.start();
+    }
+
+    public static boolean playerMoved(boolean status){
+        return status;
+    }
+}
---
+++ FabledKingdoms/src/Client/GameServer.java
@@ -0,0 +1,75 @@
+package Client;
+
+import InputHandling.InputHandler;
+import Maps.TestingMap;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class GameServer implements Runnable{
+    private Scanner reader;
+    private PrintWriter writer;
+
+    TestingMap testingMap;
+
+    GameServer(Socket socket, TestingMap testingMap) throws IOException {
+        this.reader = new Scanner(socket.getInputStream());
+        this.writer = new PrintWriter(socket.getOutputStream());
+        this.testingMap = testingMap;
+    }
+
+
+
+
+    @Override
+    public void run() {
+        try{
+            while (true){
+                String line = reader.nextLine();
+                String[] words = line.split(" ");
+
+                switch(words[0]){
+                    case "JOINED":
+                        int id = Integer.parseInt(words[1]);
+                        int playerCount = Integer.parseInt(words[2]);
+                        System.out.println("You are player " + id);
+                        testingMap.addPlayers(id, playerCount);
+                        break;
+                    case "DISCONNECTED":
+                        break;
+                    case "MAP":
+                        System.out.println("Map is " + words[1]);
+                        if (words[1].equals("TestingMap")){
+                            System.out.println("it worked");
+                            //runTestingMap();
+                        }
+                        break;
+                    case "PLAYERMOVED":
+                        int playerID = Integer.parseInt(words[1]);
+                        int xPos = Integer.parseInt(words[2]), yPos = Integer.parseInt(words[3]);
+
+                        if (playerID == 1){
+                            testingMap.p1.onlineMovement(xPos, yPos);
+                        }
+
+                        if (playerID == 2){
+                            testingMap.p2.onlineMovement(xPos,yPos);
+                        }
+                        break;
+                }
+            }
+        } catch (java.util.NoSuchElementException e){
+
+        } catch (Exception e){
+
+        }
+    }
+
+    void send(String data){
+        this.writer.println(data);
+        this.writer.flush();
+    }
+
+}
