acbb7d85c485591d00eb091fcffa57378cfbdd7b
Jordan <jordev14@gmail.com>
2020-04-03 09:20:21 +0100

    Implemented client handler and game class to process information coming from the client

---
+++ FabledKingdoms/src/Server/ClientHandler.java
@@ -0,0 +1,55 @@
+package Server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable{
+    private final Socket socket;
+    private Game game;
+
+    private Scanner reader;
+    private PrintWriter writer;
+
+    public int playerID;
+
+    public ClientHandler(Socket socket, Game game) throws IOException {
+        this.socket = socket;
+        this.game = game;
+        reader = new Scanner(socket.getInputStream());
+        writer = new PrintWriter(socket.getOutputStream());
+
+        playerID = game.newPlayer(this);
+        System.out.println("Player "+ playerID +" Connected");
+        send("JOINED " + playerID + " " + game.getPlayerCount());
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                String line = reader.nextLine();
+                //System.out.println("received: " + line);
+                String[] word = line.split(" ");
+
+                switch (word[0]){
+                    case "MAP":
+                        //System.out.println("map is " + line);
+                        send("MAP " + word[1]);
+                        break;
+                    case "POSITION":
+                        //System.out.println(this.playerID + ": xPos-" + word[1] + ", yPos-" + word[2]);
+                        //send("PLAYERMOVED " + this.playerID + " " + word[1] + " " + word[2]);
+                        game.messageOtherPlayers(this.playerID,"PLAYERMOVED " + this.playerID + " " + word[1] + " " + word[2]);
+                        break;
+                }
+            }
+        } catch (Exception e){}
+    }
+
+    void send(String data){
+        writer.println(data);
+        writer.flush();
+    }
+}
---
+++ FabledKingdoms/src/Server/Game.java
@@ -0,0 +1,49 @@
+package Server;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+//update players positions in this class
+public class Game {
+    //private ArrayList<ClientHandler> players;
+    private Map<Integer,ClientHandler> players;
+    int count, playerID;
+
+    Game(){
+       players = new HashMap<>();
+       count = 0;
+    }
+
+    int newPlayer(ClientHandler player){
+        count++;
+        //playerID = new Random().nextInt(11);
+        playerID = count;
+        if (players.containsKey(playerID)){
+            //Check if the max number of players have been added
+            //If it has then print lobby full or something like that
+            //Otherwise generate a new number
+        }
+        players.put(playerID, player);
+        return playerID;
+    }
+
+    int getPlayerCount(){
+        //System.out.println("No. of players: "+ players.size());
+        return players.size();
+    }
+
+    void directMessage(int playerID, String data){
+        players.get(playerID).send(data);
+    }
+
+    void messageOtherPlayers(int playerSending, String data){
+        for (ClientHandler c : players.values()){
+            if (c.playerID != playerSending){
+                c.send(data);
+            }
+        }
+    }
+}
